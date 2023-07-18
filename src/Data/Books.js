a38243d9b0739edcbc9489e12ee7fff3d88056cc
naatii <jcorber855@g.educaand.es>
2023-07-18 14:40:47 +0200

    Pequeño proyecto finalizado aplicando transiciones entre páginas

---
+++ src/Data/Books.js
@@ -0,0 +1,30 @@
+export const books = [
+    {
+        id: 'clean-code',
+        title: 'Clean code: Manual de desarrollo ágil de software',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+        opinion: 'Vale la pena leer la primera mitad de este libro. Entonces recordé el comentario de Martin Fowler (creo) de que el libro original Design Patterns Elements of Reusable Software fue una respuesta a las limitaciones de C++. Encajó tan bien en Java porque Java tiene muchas de las mismas limitaciones molestas y, en algunos aspectos, es incluso más difícil.',
+        author: 'Roberto C. Martín'
+    },
+    {
+        id: 'the-clean-coder',
+        title: 'The Clean Coder: A Code os Conduct for Profesional Programers',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+        opinion: 'Hay toneladas de libros similares en el mercado. El más notable es "Programador pragmático" de Andy Hunt y Dave Thomas. Ambos libros cubren un conjunto de temas bastante similar, desde aspectos de diseño hasta algunas prácticas específicas. La principal diferencia entre ellos es que el "programador pragmático" cubre un conjunto más amplio de temas y con mucha más profundidad.',
+        author: 'Robert C. Martin'
+    },
+    {
+        id: 'javascript-the-good-parts',
+        title: 'Javascript: The Good Parts',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+        opinion: 'El libro de Crockford ya se considera uno de los libros clásicos de JS y estoy de acuerdo. No porque esté completo (solo toca el núcleo de ECMAScript, ni siquiera DOM), o porque sea particularmente bueno para hacer que las cosas parezcan simples (probablemente las haga parecer demasiado complicadas). Es porque aborda JavaScript de una manera completamente diferente a la de cualquier otro libro que hayas leído. Amplía tus horizontes como desarrollador de JS, te hace pensar diferente. Y tan pronto como lo termina, no solo ha aprendido algunos datos sobre cómo funciona el idioma (estos pueden ser muy pocos), su forma de pensar al respecto ha evolucionado.',
+        author: 'Douglas Crockford'
+    },
+    {
+        id: 'the-pragmatic-programer',
+        title: 'The Pragmatic Programmer: From Journeyman to Master',
+        img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+        opinion: 'Si bien muchos se quejan de saber todo lo que hay en el libro o de que está desactualizado, creo que no entienden el punto. Tal vez este libro no le haya llamado la atención en el punto en el que se encuentra desarrollando sus habilidades y oficios, pero podría hablarle a otra persona que recién comienza. Darle una calificación baja al libro porque no era lo que necesitabas es bastante falso, ya que una calificación debería ser una guía para la calidad del libro en general.',
+        author: 'Andy Hunt, Dave Thomas'
+    }
+]
